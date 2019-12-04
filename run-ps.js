77330b0b2f66966229cc3dfb2656e12ca6c34e00
JJ Merelo <jjmerelo@gmail.com>
2019-12-04 21:44:02 +0100

    Update with SO answer

--- run-ps.js
+++ run-ps.js
@@ -1,12 +1,18 @@
-const {exec} = require("child_process")
-var child = exec('powershell .\upgrade.ps1')
-var result = '';
+// from https://stackoverflow.com/questions/10179114/execute-powershell-script-from-node-js/10181488
 
-child.stdout.on('data', function(data) {
-    result += data;
+var spawn = require("child_process").spawn,child;
+child = spawn("powershell.exe",[".\\upgrade.ps1"]);
+
+child.stdout.on("data",function(data){
+    console.log("Powershell Data: " + data);
 });
 
-child.on('close', function() {
-    console.log('done');
-    console.log(result);
+child.stderr.on("data",function(data){
+    console.log("Powershell Errors: " + data);
 });
+
+child.on("exit",function(){
+    console.log("Powershell Script finished");
+});
+
+child.stdin.end(); //end input
